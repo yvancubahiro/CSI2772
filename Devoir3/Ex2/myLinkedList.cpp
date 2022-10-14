4e8a790879ed957473c0c18b3e9db23791898ae5
yvanC <yvancubahiro@gmail.com>
2022-10-14 17:58:03 -0400

    added exercice2

--- Devoir3/Ex2/myLinkedList.cpp
+++ Devoir3/Ex2/myLinkedList.cpp
@@ -39,12 +39,61 @@ int main()
 
 
 
+
+
 /*
 *add() Function*
 **/
+
 Evaluation* add(Evaluation* p, int& number)
 {
-	//VOTRE CODE VIENT ICI
+	Evaluation *evaluation = new Evaluation;
+	Evaluation newEvaluation;
+	int position;
+	bool validPosition = false;
+
+	//saisie de donnees	de student
+	cout << "Enter the name of the Student (20 characters max.) : ";
+	cin >> newEvaluation.student;
+	cout << "Enter the grade of the student : ";
+	cin >> newEvaluation.grade;
+
+	//choix de la position de student
+	if(number == 0){
+		cout << "The element will be added to the first position since it's the first one !" << endl;
+	}else{
+		cout << "Choose the place of " << newEvaluation.student << " (between 1 and " << number << ") : ";
+
+		// verification de la valeur de position
+		while (!validPosition)
+		{
+			cin >> position;
+			position;
+			if(position <= 0 || position > number){
+				cout << "The entered place is not valid, try a place between 1 and " << number << " : ";
+			}else{
+				validPosition = true;
+			}
+		}   
+	}
+
+	
+	//ajout de donnees dans le linkedList
+	if(number == 0){//si la liste est vide
+		p = new Evaluation;
+		*p = newEvaluation;
+	}else{ // s'il y a d autres elements
+		Evaluation* currentEvaluation = p;
+		for(int i = 1; i<position;i++){ // navigation a la position ou ajouter l element
+			currentEvaluation = currentEvaluation->next;
+		}
+		newEvaluation.next = currentEvaluation->next;
+		currentEvaluation->next = new Evaluation;
+		*currentEvaluation->next = newEvaluation;
+	}
+
+	number++;
+	return p;
 }
 
 
@@ -54,7 +103,43 @@ Evaluation* add(Evaluation* p, int& number)
 
 Evaluation* remove(Evaluation* p, int& number)
 {
-	//VOTRE CODE VIENT ICI
+	if(number > 0){
+		Evaluation* currentEvaluation = p;
+		
+		//Display all evaluations
+		for(int i = 1; i<=number; i++){
+			cout << "Evaluation " << i << " -------- student : " << currentEvaluation->student << " , grades : " << currentEvaluation->grade << "." << endl;
+			currentEvaluation = currentEvaluation->next;
+		}
+		
+		
+		int position = 0;
+		cout << "Choose the number of Evaluation to delete (between 1 and " << number << " ) : ";
+		cin >> position;
+		//verification de l'entree
+		while(position>number || position <1){
+			cout << "Please choose a number between 1 and "<< number <<" ) : ";
+			cin >> position;
+		}
+
+		if(number = 1 && position == 1){//s'il y a un element dans la liste
+			p = NULL;
+		}else if(position == 1){ // s'il y a plusieurs elements et qu on efface la premiere
+			p = p->next;
+		}else{ // s'il y a plusieurs elements et qu on efface un element a une postion donnee
+			currentEvaluation = p;
+			for(int i = 1;i<position-1; i++){
+				currentEvaluation = currentEvaluation->next;
+			}
+			currentEvaluation->next = (currentEvaluation->next)->next;
+			
+		}
+		number--;
+		cout << "deleted ! " << endl;
+	}else{
+		cout << "No evaluation to delete ! " << endl;
+	}
+	return p;
 }
 
 
@@ -64,9 +149,21 @@ Evaluation* remove(Evaluation* p, int& number)
 **/
 void display(Evaluation* p)
 {
-	//VOTRE CODE VIENT ICI
-}
+	if(p != NULL){//si la liste n est pas vide
 
+		Evaluation* currentEvaluation = p;
+		int i = 1;
+		while(true){ // naviger a travers de la liste
+			cout << "Evaluation " << i++ << " -------- student : " << currentEvaluation->student << " , grades : " << currentEvaluation->grade << endl;
+			currentEvaluation = currentEvaluation->next;
+			if(currentEvaluation->next == NULL){
+				break;
+			}
+		}
+	}else{ //si la liste est vide
+		cout << "No Evaluation to display !" << endl;
+	}
+}
 
 
 /**
@@ -74,7 +171,20 @@ void display(Evaluation* p)
 ***/
 int average(Evaluation* p, int const& nbre)
 {
-	//VOTRE CODE VIENT ICI
+	int average = 0;
+	if(nbre == 0){ //si la liste est vide
+		cout << "No evaluation to calculate average" << endl;
+		return 0;
+	}else{ // s il ya plusieurs elements
+		Evaluation *currentEvaluation = p;
+		for(int i = 0; i<nbre; i++){ // naviguer a travers de la liste
+			average += currentEvaluation->grade;
+			currentEvaluation = currentEvaluation->next;
+		}
+		average = average/nbre;
+		cout << "The average is " << average << endl;
+		return 1;
+	}
 }
 
 
