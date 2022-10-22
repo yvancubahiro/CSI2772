1fe8a9e5e88a312dc2e2b60cdbf612f66a3da7b0
Audry300 <airad057@uottawa.ca>
2022-10-22 18:34:21 -0400

    Added exercice 1

--- Devoir4/Ex1/myFile1.cpp
+++ Devoir4/Ex1/myFile1.cpp
@@ -26,3 +26,81 @@ int main() {
 	cout << " Enter a number to exit..." << endl;
 	return 0;
 }
+
+ Course::Course(int a, int b){
+	num = a;
+	hours = b;
+
+} 
+
+int Course::getHours(){
+	return hours;
+}
+
+int Course::getNum(){
+	return num;
+}
+
+Student::Student(int a, int b){
+	numID = a;
+	maxCourses = b;
+	nbCourses = 0;
+	List_grades = new int [maxCourses];
+	
+	List_courses = new Course * [maxCourses];
+
+}
+
+Student::~Student(){
+
+	delete List_courses;
+	delete List_grades;
+	
+}
+
+double Student::average(){
+	double result=0.0;
+
+	if(nbCourses>0){
+		for(int i=0;i<nbCourses;i++){
+			result+=List_grades[i];
+
+		}
+
+		result=result/nbCourses;
+	}
+	return result;
+
+}
+
+int Student::totalHours(){
+	int total=0;
+
+	if(nbCourses>0){
+		for(int i=0;i<nbCourses;i++){
+			total+=List_courses[i]->getHours();
+		}
+
+	}
+	
+	return total;
+
+}
+
+bool Student::addCourse(Course* adr, int max){
+
+	bool result=false;
+
+	if(nbCourses<35){
+		List_courses[nbCourses]=adr;
+		List_grades[nbCourses]= max;
+		nbCourses++;
+		result=true;
+
+	}
+
+	return result;
+	
+
+
+}
